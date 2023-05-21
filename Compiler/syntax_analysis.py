38abb312784ee930ed6750034a426f405e671d61
Alba <alba__22@outlook.com>
2023-05-20 21:33:49 -0300

    feat: add tree to syntax analysis

--- Compiler/syntax_analysis.py
+++ Compiler/syntax_analysis.py
@@ -1,12 +1,18 @@
 from stack import Stack
+from treelib import Tree
 
 
 tokens = ["a", "b", "c", "d", "a", "d", "$"]
 
+DEBUG = False
+COUNT = 0
 
-def lexer(token_num):
-    """aaa"""
-    token = tokens[token_num]
+
+def lexer():
+    """Mocked lexer"""
+    global COUNT
+    token = tokens[COUNT]
+    COUNT += 1
     return token
 
 
@@ -34,52 +40,83 @@ def parser():
             "c": [Symbol("c", True)],
         },
     }
-    stack = Stack()
+
+    # Initial production to start the syntatic analysis
     initial_symbol = Symbol(
         value="S",
         is_terminal=False,
     )
-    stack.push(initial_symbol)
-    token_num = 0
-    next_token = lexer(token_num)
 
-    token_num += 1
+    # Creation of stack
+    stack = Stack()
+    stack.push(initial_symbol)
+
+    # Creation of tree
+    tree = Tree()
+    node = tree.create_node(tag=initial_symbol.value)
+    initial_symbol.uuid = node.identifier
+
+    next_token = lexer()
+
     while stack.is_not_empty():
+        # Take element at the top of the stack
         symbol = stack.peek()
-        print(f"=> Stack: {symbol.value}({symbol.is_terminal}) | Input: {next_token}")
+
+        debug_print(f"Stack Top: {symbol.value} | Input token: {next_token}")
 
         if symbol.is_terminal:
             if symbol.value == next_token:
-                print("Match symbol with next token")
+                debug_print("Match symbol with next token")
                 popped = stack.pop()
-                print(f"Popped: {popped.value}")
-                next_token = lexer(token_num)
-                token_num += 1
+                debug_print(f"Popped: {popped.value}")
+                next_token = lexer()
             else:
-                print("error")
-                break
+                print(f"Syntax Error: unexpected token from entry ({next_token})")
+                return None
+
         else:
+            debug_print("Not terminal, searching for valid production")
             symbols = table.get(symbol.value, {}).get(next_token, None)
 
             if symbols is None:
-                print("Error")
+                print(f"Syntax Error: unexpected token from entry ({next_token})")
                 break
-            else:
-                popped = stack.pop()
-                print(f"Popped: {popped.value}")
-                copy_symbols = symbols.copy()
-                copy_symbols.reverse()
-                for s in copy_symbols:
-                    print(f"Pushing: {s.value}")
-                    stack.push(s)
+
+            popped = stack.pop()
+            debug_print(f"Popped: {popped.value}")
+
+            # Copy needed bc of reverse is a destructive method
+            # and change the table by reference passing
+            copied_symbols = symbols.copy()
+            copied_symbols.reverse()
+
+            # Push all symbols from production in reverse order
+            # and add to tree as children of symbol
+            for child_symbol in copied_symbols:
+                debug_print(f"Pushing: {child_symbol.value}")
+                node = tree.create_node(tag=child_symbol.value, parent=symbol.uuid)
+                child_symbol.uuid = node.identifier
+                stack.push(child_symbol)
+
     if next_token != "$":
-        print("Error")
+        print(f"Syntax Error: unexpected token from entry ({next_token})")
+        return None
     else:
-        print("Recognized")
+        print("Recognized Input sequence")
+        tree.show()
+        return tree
 
 
 # Estrutura de dados:
 # Deverá representar tanto terminais quanto não-terminais
+
+
+def debug_print(value: str):
+    """
+    A function to print if debug parameter is true
+    """
+    if DEBUG:
+        print(value)
 
 
 class Symbol:
@@ -87,6 +124,7 @@ class Symbol:
     Class to represent a terminal or non-terminal symbol
     """
 
+    uuid: str
     value: str
     is_terminal: bool
 
@@ -97,3 +135,8 @@ class Symbol:
 
 if __name__ == "__main__":
     parser()
+
+
+# IDEIA: Criar um arquivo que conterá as variáveis globais(count de linha, coluna, etc.)
+# Dessa forma, é possível colocar o analisador sintático e léxico em arquivos diferentes
+# e ter uma organização melhor
