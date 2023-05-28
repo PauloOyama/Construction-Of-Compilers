c925ce5a816822eb2a8ed851d0d21135d9027ccb
Alba <alba__22@outlook.com>
2023-05-28 15:29:39 -0300

    feat: add real predictive analysis table
    still needing a better definition of symbol from lexer

--- Compiler/lexer.py
+++ Compiler/lexer.py
@@ -8,87 +8,84 @@ buffer = Buffer()
 ASCII_CHARS = string.ascii_uppercase + string.ascii_lowercase
 ASCII_DIGITS = string.digits
 
-#example to run : python lexer.py text.txt
+# example to run : python lexer.py text.txt
 
-def buffer_load(file:str) -> str:
-    """
-        Load Buffer and put guard in the last character
-    """
-    
-    with open(file,'r') as file_code:
 
+def buffer_load(file: str) -> str:
+    """
+    Load Buffer and put guard in the last character
+    """
+
+    with open(file, "r") as file_code:
         buffer = file_code.read(BUFFER_SIZE)
-        buffer = buffer + '$'
-        #Nao apagar linha abaixo talvez seja bom para testes
+        buffer = buffer + "$"
+        # Nao apagar linha abaixo talvez seja bom para testes
         # buffer = repr(buffer)
         print(len(buffer))
 
         return buffer
 
-#Codificao Direta
-def getToken(buffer: Buffer):
 
+# Codificao Direta
+def get_token(buffer: Buffer):
     MACHINE_STATE = 0
     while True:
         c = buffer.nextChar
 
-        if MACHINE_STATE == 0: 
-            if c == '=':
+        if MACHINE_STATE == 0:
+            if c == "=":
                 MACHINE_STATE = 1
-            elif c == '/':
+            elif c == "/":
                 MACHINE_STATE = 4
-            elif c == '<':
+            elif c == "<":
                 MACHINE_STATE = 9
-            elif c == '>':
+            elif c == ">":
                 MACHINE_STATE = 13
-            elif c in ASCII_CHARS: 
+            elif c in ASCII_CHARS:
                 MACHINE_STATE = 16
             elif c == " " or c == "\t" or c == "\n":
                 MACHINE_STATE = 18
-            elif c == '(':
+            elif c == "(":
                 MACHINE_STATE = 20
-            elif c == ')':
+            elif c == ")":
                 MACHINE_STATE = 21
-            elif c == '+':
+            elif c == "+":
                 MACHINE_STATE = 22
-            elif c == '-':
+            elif c == "-":
                 MACHINE_STATE = 23
-            elif c == '*':
+            elif c == "*":
                 MACHINE_STATE = 24
-            elif c == '^':
+            elif c == "^":
                 MACHINE_STATE = 43
-            elif c == '}':
+            elif c == "}":
                 MACHINE_STATE = 25
-            elif c == '{':
+            elif c == "{":
                 MACHINE_STATE = 26
-            elif c == ';':
+            elif c == ";":
                 MACHINE_STATE = 27
-            elif c == 'seila':
+            elif c == "seila":
                 MACHINE_STATE = 28
             elif c in ASCII_DIGITS:
                 MACHINE_STATE = 31
-            elif c == ',':
+            elif c == ",":
                 MACHINE_STATE = 41
-            elif c == ':':
+            elif c == ":":
                 MACHINE_STATE = 42
 
-
-
         elif MACHINE_STATE == 1:
-            if c == '=': 
+            if c == "=":
                 MACHINE_STATE = 2
             else:
                 MACHINE_STATE = 3
 
-
         elif MACHINE_STATE == 2:
             return buffer.sync()
-        
+
         elif MACHINE_STATE == 3:
             return buffer.sync()
 
         elif MACHINE_STATE == 4:
-            if c != '*':
+            if c != "*":
                 MACHINE_STATE = 5
             else:
                 MACHINE_STATE = 6
@@ -97,11 +94,11 @@ def getToken(buffer: Buffer):
             return buffer.sync()
 
         elif MACHINE_STATE == 6:
-            if c == '*':
+            if c == "*":
                 MACHINE_STATE = 7
 
         elif MACHINE_STATE == 7:
-            if c == '/':
+            if c == "/":
                 MACHINE_STATE = 8
 
         elif MACHINE_STATE == 8:
@@ -116,11 +113,11 @@ def getToken(buffer: Buffer):
         elif MACHINE_STATE == 16:
             if c not in ASCII_CHARS:
                 return buffer.sync()
-            
+
         elif MACHINE_STATE == 18:
             if c != " " and c != "\t" and c != "\n":
                 return buffer.sync()
-            
+
         elif MACHINE_STATE == 20:
             pass
         elif MACHINE_STATE == 21:
@@ -145,24 +142,19 @@ def getToken(buffer: Buffer):
             pass
         elif MACHINE_STATE == 42:
             pass
-        
+
     # break
 
-    
 
 buffer.load = buffer_load(sys.argv[1])
 
 
-print(getToken(buffer))
-print(getToken(buffer))
-print(getToken(buffer))
-print(getToken(buffer))
-print(getToken(buffer))
-print(getToken(buffer))
+print(get_token(buffer))
+print(get_token(buffer))
+print(get_token(buffer))
+print(get_token(buffer))
+print(get_token(buffer))
+print(get_token(buffer))
 # print(buffer.nextChar)
 # print(buffer.nextChar)
 # print(buffer.vigilant.where)
-
-
-
-
--- Compiler/parser.py
+++ Compiler/parser.py
@@ -2,9 +2,272 @@ from stack import Stack
 from treelib import Tree
 
 
-tokens = ["a", "b", "c", "d", "a", "d", "$"]
+class Symbol:
+    """
+    Class to represent a terminal or non-terminal symbol
+    """
 
-DEBUG = False
+    uuid: str
+    value: str
+    is_terminal: bool
+
+    def __init__(self, value, is_terminal):
+        self.value = value
+        self.is_terminal = is_terminal
+
+
+PROD_VAZIA = []
+PROD1 = [
+    Symbol("function", True),
+    Symbol("ID", True),
+    Symbol("(", True),
+    Symbol(")", True),
+    Symbol("BLOCO", False),
+]
+PROD2 = [
+    Symbol("{", True),
+    Symbol("DECLARACOES_VARS", False),
+    Symbol("SEQ_COMANDOS", False),
+    Symbol("}", True),
+]
+PROD3 = [
+    Symbol("DECLARACAO_VAR", False),
+    Symbol("DECLARACOES_VARS", False),
+]
+PROD4 = PROD_VAZIA
+PROD5 = [
+    Symbol("TIPO", False),
+    Symbol(":", True),
+    Symbol("LISTA_IDS", False),
+    Symbol(";", True),
+]
+PROD6 = [
+    Symbol("int", True),
+]
+PROD7 = [
+    Symbol("float", True),
+]
+PROD8 = [
+    Symbol("char", True),
+]
+PROD9 = [
+    Symbol("ID", True),
+    Symbol("LISTA_IDS'", False),
+]
+PROD10 = [
+    Symbol(",", True),
+    Symbol("ID", True),
+    Symbol("LISTA_IDS'", False),
+]
+PROD11 = PROD_VAZIA
+PROD12 = [
+    Symbol("COMANDO", False),
+    Symbol("SEQ_COMANDOS", False),
+]
+PROD13 = PROD_VAZIA
+PROD14 = [Symbol("CMD_ATRIB", False)]
+PROD15 = [Symbol("CMD_REPET1", False)]
+PROD16 = [Symbol("CMD_REPET2", False)]
+PROD17 = [Symbol("CMD_SELECAO", False)]
+PROD18 = [
+    Symbol("ID", True),
+    Symbol("=", True),
+    Symbol("EXPRESSAO", False),
+    Symbol(";", True),
+]
+PROD19 = [
+    Symbol("enquanto", True),
+    Symbol("(", True),
+    Symbol("COND", False),
+    Symbol(")", True),
+    Symbol("faca", True),
+    Symbol("CMD_OU_BLOCO", False),
+]
+PROD20 = [
+    Symbol("repita", True),
+    Symbol("CMD_OU_BLOCO", False),
+    Symbol("ate", True),
+    Symbol("(", True),
+    Symbol("COND", False),
+    Symbol(")", True),
+]
+PROD21 = [
+    Symbol("se", True),
+    Symbol("(", True),
+    Symbol("COND", False),
+    Symbol(")", True),
+    Symbol("CMD_OU_BLOCO", False),
+    Symbol("CMD_SELECAO'", False),
+]
+PROD22 = [Symbol("senao", True), Symbol("CMD_OU_BLOCO", False)]
+PROD23 = PROD_VAZIA
+PROD24 = [Symbol("COMANDO", False)]
+PROD25 = [Symbol("BLOCO", False)]
+PROD26 = [
+    Symbol("EXPRESSAO", False),
+    Symbol("op_rel", True),
+    Symbol("EXPRESSAO", False),
+]
+PROD27 = [Symbol("TERMO", False), Symbol("EXPRESSAO'", False)]
+PROD28 = [Symbol("+", True), Symbol("TERMO", False), Symbol("EXPRESSAO'", False)]
+PROD29 = [Symbol("-", True), Symbol("TERMO", False), Symbol("EXPRESSAO'", False)]
+PROD30 = PROD_VAZIA
+PROD31 = [Symbol("EXPONENC", False), Symbol("TERMO'", False)]
+PROD32 = [Symbol("*", True), Symbol("EXPONENC", False), Symbol("TERMO'", False)]
+PROD33 = [Symbol("/", True), Symbol("EXPONENC", False), Symbol("TERMO'", False)]
+PROD34 = PROD_VAZIA
+PROD35 = [Symbol("FATOR", False), Symbol("EXPONENC'", False)]
+PROD36 = [Symbol("^", True), Symbol("FATOR", False), Symbol("EXPONENC'", False)]
+PROD37 = PROD_VAZIA
+PROD38 = [Symbol("(", True), Symbol("EXPRESSAO", False), Symbol(")", True)]
+PROD39 = [Symbol("CONST_CHAR", True)]
+PROD40 = [Symbol("CONST_NUM", True)]
+PROD41 = [Symbol("ID", True)]
+
+table = {
+    "INICIO": {
+        "function": PROD1,
+    },
+    "BLOCO": {
+        "{": PROD2,
+    },
+    "DECLARACOES_VARS": {
+        "int": PROD3,
+        "char": PROD3,
+        "float": PROD3,
+        "senao": PROD4,
+        "enquanto": PROD4,
+        "repita": PROD4,
+        "ID": PROD4,
+    },
+    "DECLARACAO_VAR": {
+        "int": PROD5,
+        "char": PROD5,
+        "float": PROD5,
+    },
+    "TIPO": {
+        "int": PROD6,
+        "char": PROD7,
+        "float": PROD8,
+    },
+    "LISTA_IDS": {
+        "ID": PROD9,
+    },
+    "LISTA_IDS'": {
+        ",": PROD10,
+        ";": PROD11,
+    },
+    "SEQ_COMANDOS": {
+        "se": PROD12,
+        "senao": PROD12,
+        "enquanto": PROD12,
+        "repita": PROD12,
+        "ID": PROD12,
+        "}": PROD13,
+    },
+    "COMANDO": {
+        "ID": PROD14,
+        "enquanto": PROD15,
+        "repita": PROD16,
+        "se": PROD17,
+    },
+    "CMD_ATRIB": {
+        "ID": PROD18,
+    },
+    "CMD_REPET1": {
+        "enquanto": PROD19,
+    },
+    "CMD_REPET2": {
+        "repita": PROD20,
+    },
+    "CMD_SELECAO": {
+        "se": PROD21,
+    },
+    "CMD_SELECAO'": {
+        "senao": PROD22,
+        "se": PROD23,
+        "enquanto": PROD23,
+        "repita": PROD23,
+        "ID": PROD23,
+    },
+    "CMD_OU_BLOCO": {
+        "senao": PROD24,
+        "se": PROD24,
+        "enquanto": PROD24,
+        "repita": PROD24,
+        "ID": PROD24,
+        "{": PROD25,
+    },
+    "COND": {
+        "(": PROD26,
+        "ID": PROD26,
+        "CONST_CHAR": PROD26,
+        "CONST_NUM": PROD26,
+    },
+    "EXPRESSAO": {
+        "(": PROD27,
+        "ID": PROD27,
+        "CONST_CHAR": PROD27,
+        "CONST_NUM": PROD27,
+    },
+    "EXPRESSAO'": {
+        "+": PROD28,
+        "-": PROD29,
+        ")": PROD30,
+        "op_rel": PROD30,
+        ";": PROD30,
+    },
+    "TERMO": {
+        "(": PROD31,
+        "ID": PROD31,
+        "CONST_CHAR": PROD31,
+        "CONST_NUM": PROD31,
+    },
+    "TERMO'": {
+        "*": PROD32,
+        "/": PROD33,
+        "+": PROD34,
+        "-": PROD34,
+    },
+    "EXPONENC": {
+        "(": PROD35,
+        "ID": PROD35,
+        "CONST_CHAR": PROD35,
+        "CONST_NUM": PROD35,
+    },
+    "EXPONENC'": {
+        "^": PROD36,
+        "*": PROD37,
+        "/": PROD37,
+    },
+    "FATOR": {
+        "(": PROD38,
+        "ID": PROD41,
+        "CONST_CHAR": PROD39,
+        "CONST_NUM": PROD40,
+    },
+}
+
+
+tokens = [
+    "function",
+    "main",
+    "(",
+    ")",
+    "{",
+    "int",
+    ":",
+    "x",
+    ",",
+    "y",
+    ",",
+    "z",
+    ";",
+    "}",
+    "$",
+]
+
+DEBUG = True
 COUNT = 0
 
 
@@ -21,29 +284,9 @@ def parser():
     Function to parse the given code and return the syntax tree
     """
 
-    table = {
-        "S": {
-            "a": [Symbol("A", False), Symbol("S", False)],
-            "b": [Symbol("B", False), Symbol("A", False)],
-            "c": [Symbol("A", False), Symbol("S", False)],
-            "d": [Symbol("B", False), Symbol("A", False)],
-        },
-        "A": {
-            "a": [Symbol("a", True), Symbol("B", False)],
-            "c": [Symbol("C", False)],
-        },
-        "B": {
-            "b": [Symbol("b", True), Symbol("A", False)],
-            "d": [Symbol("d", True)],
-        },
-        "C": {
-            "c": [Symbol("c", True)],
-        },
-    }
-
     # Initial production to start the syntatic analysis
     initial_symbol = Symbol(
-        value="S",
+        value="INICIO",
         is_terminal=False,
     )
 
@@ -101,10 +344,10 @@ def parser():
     if next_token != "$":
         print(f"Syntax Error: unexpected token from entry ({next_token})")
         return None
-    else:
-        print("Recognized Input sequence")
-        tree.show()
-        return tree
+
+    print("Recognized Input sequence")
+    tree.show()
+    return tree
 
 
 # Estrutura de dados:
@@ -117,20 +360,6 @@ def debug_print(value: str):
     """
     if DEBUG:
         print(value)
-
-
-class Symbol:
-    """
-    Class to represent a terminal or non-terminal symbol
-    """
-
-    uuid: str
-    value: str
-    is_terminal: bool
-
-    def __init__(self, value, is_terminal):
-        self.value = value
-        self.is_terminal = is_terminal
 
 
 if __name__ == "__main__":
